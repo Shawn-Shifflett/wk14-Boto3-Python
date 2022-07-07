abbac05757a7fe61b5333453254056d58116a964
Shawn-Shifflett <shifflettko@battlers.ab.edu>
2022-07-07 02:31:35 +0000

    create bucket

---
+++ createS3.py
@@ -0,0 +1,11 @@
+import boto3
+
+awsResource = boto3.resource("s3") # S3 bucket obj
+bucket = awsResource.Bucket("boto3pract1") #giving the bucket a name
+
+#create bucket
+response = bucket.create(
+    ACL='private',
+)
+
+print(response)
